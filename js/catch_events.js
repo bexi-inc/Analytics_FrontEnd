f72949d44338879f666000ecbacfce16041c543c
chrisgarcia96 <60152967+chrisgarcia96@users.noreply.github.com>
2020-01-28 11:51:25 -0700

    join files
    union de archivos

--- js/catch_events.js
+++ js/catch_events.js
@@ -1,3 +1,118 @@
+class Scroll{
+    constructor(){
+        this.winheight = 0
+        this.docheight = 0
+        this.trackLength = 0
+        this.pctAux = 0
+    }
+
+    getmeasurements(){
+        this.winheight = $(window).height()
+        this.docheight = $(document).height()
+        this.trackLength = this.docheight - this.winheight
+    }
+
+    amountscrolled(){ //scroll percentage
+        var pctScrolled = 0
+        var scrollTop = $(window).scrollTop()
+        pctScrolled = Math.floor(scrollTop/this.trackLength * 100) // gets percentage scrolled (ie: 80 NaN if tracklength == 0)
+        return pctScrolled
+    }
+
+}
+
+
+class Timer{
+    constructor(callback, delay){
+        this.callback = callback;
+        this.timerId, this.start, this.remaining = delay;
+    }
+
+    startTimer(){
+        this.resume();
+    }
+
+    pause() {
+        window.clearTimeout(this.timerId);
+        this.remaining -= new Date() - this.start;
+    };
+
+    resume() {
+        this.start = new Date();
+        window.clearTimeout(this.timerId);
+        this.timerId = window.setTimeout(this.callback, this.remaining);
+    };
+
+    reset() {
+        clearTimeout(this);
+        this.startTimer();
+    }
+}
+
+class visibilityChange {
+
+    constructor(){
+        this.prefix = visibilityChange.getBrowserPrefix()
+        this.hidden = visibilityChange.hiddenProperty(this.prefix)
+        this.visibilityState = visibilityChange.visibilityState(this.prefix)
+        this.visibilityEvent = visibilityChange.visibilityEvent(this.prefix)
+    }
+
+    get _visibilityEvent(){
+        return this.visibilityEvent
+    }
+
+    get _hidden(){
+        return this.hidden
+    }
+
+    static getBrowserPrefix() {     
+        if ('hidden' in document) {
+            return null
+        }
+        
+        var browserPrefixes = ['moz', 'ms', 'o', 'webkit']
+
+        for (var i = 0; i < browserPrefixes.length; i++) {
+            var prefix = browserPrefixes[i] + 'Hidden'
+            if (prefix in document) {
+                return browserPrefixes[i]
+            }
+        }
+
+        return null
+    }
+
+    
+    static hiddenProperty(prefix) {
+        if (prefix) {
+            return prefix + 'Hidden'
+        } else {
+            return 'hidden'
+        }
+    }
+
+    
+    static visibilityState(prefix) {
+        if (prefix) {
+            return prefix + 'VisibilityState'
+        } else {
+            return 'visibilityState'
+        }
+    }
+
+    
+    static visibilityEvent(prefix) {
+        if (prefix) {
+            return prefix + 'visibilitychange'
+        } else {
+            return 'visibilitychange'
+        }
+    }
+}
+
+
+
 var visibility_change = new visibilityChange()
 var scroll_change = new Scroll()
 var timer = null
@@ -51,7 +166,7 @@ window.addEventListener("scroll", function(){
          if(pctScrolled > pctAux){
             pctAux = pctScrolled
             b = new baw("test-1234","scrollPercentage",{scrollPercentage : pctScrolled});
-            //console.log(pctScrolled)  
+            console.log(pctScrolled)  
         } 
     }, 50)
 }, false)
