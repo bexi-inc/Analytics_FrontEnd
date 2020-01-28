0fc6e047b6b90785954bab7841e95e9996339d3f
chrisgarcia96 <60152967+chrisgarcia96@users.noreply.github.com>
2020-01-28 11:52:29 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
  <head>
  	<title>Analytics Test</title>
  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	
-	 <script src="js/timer.js"></script>
-    <script type="text/javascript" src="js/visibility_change.js"></script>
-    <script type="text/javascript" src="js/scroll.js"></script>
     
     <script type="text/javascript" src="js/bexi_analytics.js"></script>
 
