33c76747dbe135c4e1d7cfd5eb4776fedc5a219a
Adryx46 <adrian@bexi.io>
2020-01-31 17:53:46 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,13 @@
  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     
     <script type="text/javascript" src="js/bexi_analytics.js"></script>
-    <script type="text/javascript">
-    	b = new baw("test-1234");
-    </script>
-    
-	
  </head>
  <body style="height: 900px;overflow-x: scroll;">
     <p id="log">Click on this document to give it focus.</p>
     <button class="bexi_button">presiona</button><br>
     <a href="#" class="bexi_button">presiona</a>
 </body>
+<script type="text/javascript">
+    	b = new baw("test-1234");
+</script>
  </html>
