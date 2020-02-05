2a08bc11f892ed03f9877df320a2dd9ff82f217f
Manuel Cuevas <manuel@bexi.io>
2020-02-05 10:40:04 -0700

    Enable Cors

--- api/analytics.php
+++ api/analytics.php
@@ -1,4 +1,8 @@
 <?
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 session_start();
 
 require 'vendor/autoload.php';
