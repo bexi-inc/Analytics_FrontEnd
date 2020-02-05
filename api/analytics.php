911517419fafedb254897660556b33caed2c9e38
Adryx46 <adrian@bexi.io>
2020-02-05 11:29:11 -0700

    Update analytics.php

--- api/analytics.php
+++ api/analytics.php
@@ -1,6 +1,6 @@
 <?
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Set-Cookie, Cookie, Bearer");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Credentials: true");
